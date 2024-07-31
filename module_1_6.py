0b05f453b24392e0b25528ed65280ec3a8d2974f
OakLotu <danyakalinin1201@gmail.com>
2024-07-31 16:52:44 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,11 @@
+my_dict = {'Danya': 2008}
+print (my_dict)
+print (my_dict['Danya'])
+my_dict['Vasya'] = 2012
+print (my_dict)
+del my_dict['Danya']
+print (my_dict)
+my_set = {12, 1, 2008, 'Danya', 12}
+print(my_set)
+my_set.add('Vasya')
+my_set.discard(12)
